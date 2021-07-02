61469c2b28e0d51dcc14d4a74728b74355e80e21
Lucas Ellery <lucas.ellery@somapay.com.br>
2021-07-02 11:51:07 -0300

    feature: add button added to screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,11 +3,13 @@ import { View } from 'react-native';
 import { styles } from './styles';
 
 import { Profile } from '../../components/Profile'
+import { ButtonAdd } from '../../components/ButtonAdd'
 
 export const Home: React.FC = () => {
   return (
     <View style={styles.header}>
       <Profile />
+      <ButtonAdd />
     </View>
   );
 }
